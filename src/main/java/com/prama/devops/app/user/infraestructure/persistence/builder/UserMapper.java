b0df96fa9c4910e2d5b588ee7b217ebf8911f036
jonatan.restrepo <jonatan.restrepo@pragma.com.co>
2023-07-21 09:13:39 -0500

    feat: test
    implementacion test

--- src/main/java/com/prama/devops/app/user/infraestructure/persistence/builder/UserMapper.java
+++ src/main/java/com/prama/devops/app/user/infraestructure/persistence/builder/UserMapper.java
@@ -3,6 +3,7 @@ package com.prama.devops.app.user.infraestructure.persistence.builder;
 import com.prama.devops.app.user.domain.model.User;
 import com.prama.devops.app.user.infraestructure.persistence.entity.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.stream.Stream;
 
@@ -11,5 +12,6 @@ public interface UserMapper {
     User userEntityToUser(UserEntity entity);
     Stream<User> userEntitiesToUsers(Stream<UserEntity> userEntityStream);
 
+    @Mapping(target = "id", ignore = true)
     UserEntity userToUserEntity(User user);
 }
