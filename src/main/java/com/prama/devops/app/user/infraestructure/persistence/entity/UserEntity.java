b9f2c76ee8c07be43bbf55f03efe286c37329e0c
jonatan.restrepo <jonatan.restrepo@pragma.com.co>
2023-07-24 14:25:45 -0500

    feat: reportes v1.2
    implementacion de reportes

--- src/main/java/com/prama/devops/app/user/infraestructure/persistence/entity/UserEntity.java
+++ src/main/java/com/prama/devops/app/user/infraestructure/persistence/entity/UserEntity.java
@@ -1,59 +1,22 @@
 package com.prama.devops.app.user.infraestructure.persistence.entity;
 
+import com.prama.devops.app.user.domain.model.User;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "user")
-public class UserEntity {
+public class UserEntity extends User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String identification;
-    private String name;
-    private String lastName;
-    private String sex;
 
     public UserEntity(Long id, String identification, String name, String lastName, String sex) {
+        super(identification, name, lastName, sex);
         this.id = id;
-        this.identification = identification;
-        this.name = name;
-        this.lastName = lastName;
-        this.sex = sex;
     }
 
     public UserEntity(){}
-
-    public String getIdentification() {
-        return identification;
-    }
-
-    public void setIdentification(String identification) {
-        this.identification = identification;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
 
     public void setId(Long id) {
         this.id = id;
