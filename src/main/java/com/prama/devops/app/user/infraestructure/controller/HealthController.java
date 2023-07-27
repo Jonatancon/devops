a4d2f7aa0186c3e76af6639584d9514bb488c3e9
jonatan.restrepo <jonatan.restrepo@pragma.com.co>
2023-07-26 23:44:46 -0500

    feat: health container
    health container for ECS

---
+++ src/main/java/com/prama/devops/app/user/infraestructure/controller/HealthController.java
@@ -0,0 +1,19 @@
+package com.prama.devops.app.user.infraestructure.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+@Log4j
+public class HealthController {
+
+    @GetMapping
+    public HttpEntity<?> ok() {
+        return  ResponseEntity.EMPTY;
+    }
+}
